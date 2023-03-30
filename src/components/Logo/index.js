8ed681720846476ae31e4502ddd01817ddc6ab13
Danjuma011 <ayanfolarin@gmail.com>
2023-03-30 18:07:04 +0100

    back to normal

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -1,48 +1,49 @@
 import './index.scss'
-import Logo5 from '../../assets/Logo/logo-5.png'
+// import Logo5 from '../../assets/Logo/logo-5.png'
 import { useEffect, useRef } from 'react'
+ import Logo5 from "../../assets/Logo/logo-5.png";
 
 const Logo = () => {
   const bgRef = useRef();
-  const outlineLogoRef = useRef();
-  const outlineLogoRef2 = useRef();
+   const outlineLogoRef = useRef();
+   const outlineLogoRef2 = useRef();
   const solidLogoRef = useRef();
 
-  useEffect(() => {
-     gsap.registerPlugin(DrawSVGPlugin)
+  // useEffect(() => {
+  //    gsap.registerPlugin(DrawSVGPlugin)
 
-    gsap.timeline()
-      .to(bgRef.current, {
-        duration: 1,
-        opacity: 1
-      })
-    .from(outlineLogoRef.current,{
-      drawSVG: 0,
-      duration: 8
-    })
-    .from(outlineLogoRef2.current,{
-      drawSVG: 0,
-      duration: 4, 
-      delay: "-8"
-    })
+  //   gsap.timeline()
+  //     .to(bgRef.current, {
+  //       duration: 1,
+  //       opacity: 1
+  //     })
+  //   .from(outlineLogoRef.current,{
+  //     drawSVG: 0,
+  //     duration: 8
+  //   })
+  //   .from(outlineLogoRef2.current,{
+  //     drawSVG: 0,
+  //     duration: 4, 
+  //     delay: "-8"
+  //   })
 
-    gsap.fromTo(
-      solidLogoRef.current,
-      {
-        opacity: 0,
-      },
-      {
-        opacity: 1,
-        delay: 3,
-        duration: 4,
-      }
-    )
+  //   gsap.fromTo(
+  //     solidLogoRef.current,
+  //     {
+  //       opacity: 0,
+  //     },
+  //     {
+  //       opacity: 1,
+  //       delay: 3,
+  //       duration: 4,
+  //     }
+  //   )
 
-  }, [])
+  // }, [])
 
   return (
     <div className='logo-container' ref={bgRef}>
-      <img ref={solidLogoRef} className='solid-logo' src={Logo5} alt="S" />
+      <img  className='solid-logo' src={Logo5} alt="S" />
 
       <svg
         width="114"
